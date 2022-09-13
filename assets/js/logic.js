1dd11edfe19c3f38cce7e50fccf7bed6fce3a24c
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-09-13 02:59:10 -0500

    created function to get weather information for the next fives days and add the elements to html

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,14 +4,14 @@ let userLocation = $('#city-search');
 let searchSubmit = $('#search-btn');
 let weatherToday = $('#weather-today');
 let todayDisplay = $('#display-today');
+let forecastContainer = $('#yugioh-cards');
 
+//this function gets the weather information for the current day and creates elements dynamically to the html 
 function getWeather(event){
     event.preventDefault();
     $('#display-today p').remove();
     let userCity = userLocation.val();
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&APPID=${apiKey}&units=imperial`;
-    // console.log(userCity);
-    // location.replace('./index.html?'+`q=${userCity}`)
     fetch(apiUrl)
     .then(function(response){
         return response.json();
@@ -24,23 +24,55 @@ function getWeather(event){
             alt: 'weather-icon'
         });
         let temp = $('<p>').text(`Temp: ${results.main.temp}°F`);
-        let humid = $('<p>').text(`Humidity: ${results.main.humidity} %`);
         let windSpeed = $('<p>').text(`Wind: ${results.wind.speed} MPH`);
+        let humid = $('<p>').text(`Humidity: ${results.main.humidity} %`);
         weatherToday.text(`${results.name} (${moment().format('L')})`);
         weatherToday.append(iconImage); 
         todayDisplay.append(temp, windSpeed, humid);
-        todayDisplay.attr('class', 'border border-dark');
-
-    })
-    
+        todayDisplay.attr('class', 'border border-dark p-3');
+        getFiveDay();
+    })   
 }
+//this function pulls the weather information we need for the next five days and creates the elements dynamically on the html
+function getFiveDay(){
+    let userCity = userLocation.val();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${userCity}&appid=${apiKey}&units=imperial`;
+    fetch(apiUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(results){
+        forecastContainer.empty();
+        let fiveDayArr = [];
+        fiveDayArr.push(results.list[7]);
+        fiveDayArr.push(results.list[15]);
+        fiveDayArr.push(results.list[23]);
+        fiveDayArr.push(results.list[31]);
+        fiveDayArr.push(results.list[39]);
+        console.log(fiveDayArr);
+        for(let i = 0; i < fiveDayArr.length; i++){
+            let temp = $('<p>').text(`Temp: ${fiveDayArr[i].main.temp}°F`);
+            let windSpeed = $('<p>').text(`Wind: ${fiveDayArr[i].wind.speed} MPH`);
+            let humid = $('<p>').text(`Wind: ${fiveDayArr[i].main.humidity} %`);
+            let weatherIcon = fiveDayArr[i].weather[0].icon;
+            let iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}.png`
+            let iconImage = $('<img>',{
+                src: `${iconUrl}`,
+                alt: 'weather-icon'
+            })
+            let dateArr = fiveDayArr[i].dt_txt.split(" ")[0].split('-');
+            let cardName = $('<h5>').text(`${dateArr[1]}/${dateArr[2]}/${dateArr[0]}`);
+            cardName.attr('class','card-title');
+            let cardBody = $('<div>').attr('class', 'card-body bg-dark text-white');
+            let cardContainer = $('<div>',{
+                class: 'card',
+                style: 'width: 18rem;'
+            });
+            cardBody.append(cardName, iconImage, temp, windSpeed, humid);
+            cardContainer.append(cardBody);
+            forecastContainer.append(cardContainer);
+        }
+    })
+}    
 
-
-
-
-// console.log(results);
-// console.log(results.name);
-// console.log(results.main.humidity);
-// console.log(results.wind.speed);
-// console.log(results.weather[0].icon)
 searchSubmit.on('click', getWeather)
