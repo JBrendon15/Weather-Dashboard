818b14fa38936cd024504b51aeb7160471a3b6e9
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-09-12 21:59:07 -0500

    added the search form and col for the displayed weather on css and added a function to record the user input

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,20 @@
-console.log('hello');
+const apiKey = '5e91cc2d11f23d6b9e2b68d9e5e55a0d'
+
+let userLocation = $('#city-search')
+let searchSubmit = $('#search-btn')
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userLocation}&APPID=${apiKey}`;
+
+function getWeather(event){
+    event.preventDefault();
+    let userCity = userLocation.val();
+    // console.log(userCity);
+    location.replace('./index.html?'+`q=${userCity}`)
+    // fetch(apiUrl)
+    // .then()
+}
+
+
+
+
+searchSubmit.on('click', getWeather)
